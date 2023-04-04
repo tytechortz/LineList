6d8fb5ba2aa495692be53ab2e9b6cef10486bbed
James Swank <jmswank7@gmail.com>
2023-04-04 09:04:11 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from dash import Dash, html, dcc, Input, Output, State
+import dash_bootstrap_components as dbc
+import geopandas as gpd
+import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
+
+
+app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.DARKLY])
+
+header = html.Div("Arapahoe Covid-19 Case Investigation Tool", className="h2 p-2 text-white bg-primary text-center")
+
+
+
+app.layout = dbc.Container([
+    header,
+])
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8050)
