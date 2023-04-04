bd1012a404b70c3119f43c9872a36d2447798d49
James Swank <jmswank7@gmail.com>
2023-04-04 14:21:10 -0600

    map placeholder and blank figure

--- app.py
+++ app.py
@@ -8,14 +8,33 @@ import plotly.graph_objects as go
 
 app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.DARKLY])
 
+
+bgcolor = "#f3f3f1"  # mapbox light map land color
 header = html.Div("Arapahoe Covid-19 Case Investigation Tool", className="h2 p-2 text-white bg-primary text-center")
+template = {"layout": {"paper_bgcolor": bgcolor, "plot_bgcolor": bgcolor}}
+
 
 df = pd.read_csv('/Users/jamesswank/Downloads/CSV.csv')
 print(df)
 
+def blank_fig(height):
+    """
+    Build blank figure with the requested height
+    """
+    return {
+        "data": [],
+        "layout": {
+            "height": height,
+            "template": template,
+            "xaxis": {"visible": False},
+            "yaxis": {"visible": False},
+        },
+    }
+
 
 app.layout = dbc.Container([
     header,
+    dbc.Row(dcc.Graph(id='ct-map', figure=blank_fig(500))),
 ])
 
 
