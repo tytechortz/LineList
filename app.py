bbb6a4dac24d5b6f7533689054801efa2936bd43
James Swank <jmswank7@gmail.com>
2023-04-04 14:37:26 -0600

    plots one ct

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ template = {"layout": {"paper_bgcolor": bgcolor, "plot_bgcolor": bgcolor}}
 df = pd.read_csv('/Users/jamesswank/Downloads/CSV.csv')
 print(df)
 
+gdf_2020 = gpd.read_file('2020_CT/ArapahoeCT.shp')
+gdf_2020['FIPS'] = gdf_2020['FIPS'].apply(lambda x: x[5:])
+print(gdf_2020.FIPS)
+
 def blank_fig(height):
     """
     Build blank figure with the requested height
@@ -35,7 +39,56 @@ def blank_fig(height):
 app.layout = dbc.Container([
     header,
     dbc.Row(dcc.Graph(id='ct-map', figure=blank_fig(500))),
+    dbc.Row([
+            dbc.Col([
+                dcc.Slider(0, 1, value=1,
+                    marks={
+                        0: {'label': 'Light', 'style': {'color': 'white'}},
+                        1: {'label': 'Dark', 'style': {'color': 'white'}},
+                    },
+                    id = 'opacity',
+                ),
+            ], width=6),
+        ]),
 ])
+
+
+@app.callback(
+    Output('ct-map', 'figure'),
+    # Input('year-map-data', 'data'),
+    # Input('variable-dropdown', 'value'),
+    # Input('year', 'value'),
+    Input('opacity', 'value')
+)
+def get_figure(opacity):
+  
+    # df = pd.read_json(selected_data)
+    # df['FIPS'] = df["FIPS"].astype(str)
+    # df = df_SVI_2016
+    # df['FIPS'] = df["FIPS"].astype(str)
+    
+    # selection = dropdown
+    df.rename(columns={'tract2000':'FIPS'}, inplace=True)
+    df['FIPS'] = df["FIPS"].astype(str)
+    tgdf = gdf_2020.merge(df, on='FIPS')
+    tgdf = tgdf.set_index('FIPS')
+  
+
+    fig = px.choropleth_mapbox(tgdf, 
+                                geojson=tgdf.geometry, 
+                                color=tgdf.county,                               
+                                locations=tgdf.index, 
+                                # featureidkey="properties.TRACTCE20",
+                                opacity=opacity)
+
+    fig.update_layout(mapbox_style="carto-positron", 
+                      mapbox_zoom=10.4,
+                      mapbox_center={"lat": 39.65, "lon": -104.8},
+                      margin={"r":0,"t":0,"l":0,"b":0},
+                      uirevision='constant')
+
+
+    return fig
 
 
 if __name__ == "__main__":
