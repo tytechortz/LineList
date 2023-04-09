9393be3be9607e0cdfdb5422dbe020cd1090e18b
James Swank <jmswank7@gmail.com>
2023-04-09 17:36:55 -0600

    app runs

--- app2.py
+++ app2.py
@@ -4,3 +4,46 @@ import geopandas as gpd
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
+
+app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.DARKLY])
+
+
+header = html.Div("Arapahoe Covid-19 Case Investigation Tool", className="h2 p-2 text-white bg-primary text-center")
+bgcolor = "#f3f3f1"  # mapbox light map land color
+template = {"layout": {"paper_bgcolor": bgcolor, "plot_bgcolor": bgcolor}}
+
+gdf_2020 = gpd.read_file('2020_CT/ArapahoeCT.shp')
+gdf_2020 = gdf_2020.to_crs('WGS84')
+gdf_2020['FIPS'] = gdf_2020['FIPS'].apply(lambda x: x[4:])
+
+df_SVI_2020 = pd.read_csv('Colorado_SVI_2020.csv')
+df_SVI_2020 = df_SVI_2020.loc[df_SVI_2020['COUNTY'] == 'Arapahoe']
+df_SVI_2020['FIPS'] = df_SVI_2020["FIPS"].astype(str)
+df_SVI_2020['FIPS'] = df_SVI_2020['FIPS'].apply(lambda x: x[4:])
+
+def blank_fig(height):
+    """
+    Build blank figure with the requested height
+    """
+    return {
+        "data": [],
+        "layout": {
+            "height": height,
+            "template": template,
+            "xaxis": {"visible": False},
+            "yaxis": {"visible": False},
+        },
+    }
+
+app.layout = dbc.Container([
+    header,
+    dbc.Row(dcc.Graph(id='ct-map', figure=blank_fig(500))),
+    dbc.Row([
+            
+        ]),
+        dcc.Store(id='all-map-data', storage_type='session'),
+])   
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8050)
